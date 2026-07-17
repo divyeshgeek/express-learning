e6dfc85e3529b230796a4c495bfabd42817df679
divyeshgeek <divyeshgeek435@gmail.com>
2026-07-17 13:11:55 +0530

    Add middleware to log tour ID on parameter retrieval in toursRoutes.js

--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -2,6 +2,10 @@ const express=require("express");
 const {getAllTours,createTour,getTourById,updateTourById,deleteTourById}=require("../controllers/tourController");
 
 const router=express.Router();
+router.param("id",(req,res,next,val)=>{
+    console.log(`Tour id is: ${val}`);
+    next();
+});
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTourById).patch(updateTourById).delete(deleteTourById);
 
