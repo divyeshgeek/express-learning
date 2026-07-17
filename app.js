5f13feda3de5d0a8e0fbbbddf33f1468b1379072
divyeshgeek <divyeshgeek435@gmail.com>
2026-07-17 10:29:03 +0530

    express learning

---
+++ app.js
@@ -0,0 +1,107 @@
+const express=require("express");
+const fs=require("fs");
+const app=express();
+app.use(express.json());
+// app.get("/",(req,res)=>{
+//     // res.send("Hello World");
+//     res.status(200).json({
+//         message:"Hello World",
+//         app:"Express learning",
+//         status:"success"
+//     });
+// });
+
+// app.post("/",(req,res)=>{
+//     res.status(200).json({
+//         message:"This is a post request",
+//         app:"Express learning",
+//         status:"success"
+//     });
+
+// });
+ 
+const toursData=JSON.parse(fs.readFileSync(`${__dirname}/constant/tours-data.json`,"utf-8"));
+
+const getAllTours=(req,res)=>{
+    res.status(200).json({
+        status:"success",
+        results:toursData.length,
+        data:toursData
+    });
+};
+
+const createTour=(req,res)=>{
+    console.log(req.body);
+    const newTour=Object.assign({id:toursData.length+1},req.body);
+    toursData.push(newTour);
+    fs.writeFileSync(`${__dirname}/constant/tours-data.json`,JSON.stringify(toursData));
+    res.status(200).json({
+        status:"success",
+        data:newTour
+    });
+};
+
+const getTourById=(req,res)=>{
+    const id=req.params.id;
+    const tour=toursData.find(tour=>tour.id===parseInt(id));
+    if(!tour){
+        return res.status(404).json({
+            status:"fail",
+            message:"Tour not found"
+        });
+    }
+    res.status(200).json({
+        status:"success",
+        data:tour
+    });
+};
+
+const updateTourById=(req,res)=>{
+    const id=req.params.id;
+    const tour=toursData.find(tour=>tour.id===parseInt(id));
+    if(!tour){
+        return res.status(404).json({
+            status:"fail",
+            message:"Tour not found"
+        });
+    }
+    const updatedTour=Object.assign(tour,req.body);
+    fs.writeFileSync(`${__dirname}/constant/tours-data.json`,JSON.stringify(toursData));
+    res.status(200).json({
+        status:"success",
+        data:updatedTour
+    });
+};
+
+
+const deleteTourById=(req,res)=>{
+    const id=req.params.id;
+    const tour=toursData.find(tour=>tour.id===parseInt(id));
+    if(!tour){
+        return res.status(404).json({
+            status:"fail",
+            message:"Tour not found"
+        });
+    }
+    const index=toursData.indexOf(tour);
+    toursData.splice(index,1);
+    fs.writeFileSync(`${__dirname}/constant/tours-data.json`,JSON.stringify(toursData));
+    res.status(200).json({
+        status:"success",
+        data:null
+    });
+};
+ 
+// app.get("/api/v1/tours",getAllTours);
+// app.post("/api/v1/tours",createTour);
+// app.get("/api/v1/tours/:id",getTourById);
+// app.patch("/api/v1/tours/:id",updateTourById);
+// app.delete("/api/v1/tours/:id",deleteTourById);
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+app.route("/api/v1/tours/:id").get(getTourById).patch(updateTourById).delete(deleteTourById);
+
+const port=3000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
